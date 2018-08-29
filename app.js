6b868fd205dbd6bbd20da98fd9b5809517dc7af5
SWOPE <ps0895423@otc.edu>
2018-08-29 09:43:29 -0500

    Final commit dis stuff

--- app.js
+++ app.js
@@ -1,1 +1,88 @@
+//  Here is a comment 
+/*  Multi Line
+    Phillip
+    
 console.log("Hello JavaScript");
+
+//VALUE or Data types
+console.log("Hello from JavaScrupt!");
+console.log("Let's do some math");
+console.log(4+7);
+console.log(12/0);
+console.log("Goodbye!");
+
+let a = 3.14;
+console.log(a);
+
+const temp1 = 36.9;
+const temp2 = 37.6;
+const temp3 = 37.1;
+console.log (temp1, temp2, temp3);
+//temp3 = 9;
+
+//increment a number variable
+let c=0;
+c += 1;
+c++;
+console.log(c)
+
+// Variable scope
+let num1 = 0;
+{
+    num1 = 1; //Ok : num1 is declared in the parent block
+    const num2 = 0;
+}
+
+console.log(num2); //ok : num1 is declared in the current block
+
+
+const country = "France";
+console.log(`I live in ${country}`);
+const x = 3;
+const y = 7;
+console.log(`${x} + ${y} + ${x + y}`);
+
+
+console.log(4.5);
+console.log("4+5");
+console.log("4"+"5");
+console.log(4+"5");
+const g= "five" * 2;
+console.log(g);
+
+
+
+const h ="5";
+console.log(h + 1);
+const i = Number(h);
+console.log (i + 1);
+const j = 6;
+console.log ("5" + String());
+
+console.log(Number("dog"));
+
+
+
+let num = 0;
+let str = '0';
+
+
+//Stric equality using ===
+console.log (num === num); //true
+console.log (str === str); //true
+console.log (num === str); //false
+console.log (num==str) //true
+
+*/
+
+const x = 'abc';
+switch (x){
+    case "abc":
+    console.log("x = abc");
+    //break;
+    case"def":
+    console.log ("x = def");
+    break;
+    default:
+    console.log("otherwise");
+}
